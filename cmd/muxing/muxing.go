8d68d0a73dc5620e8aac395a36ee5fb1c32d3690
Maksim Abramovich <maksim_abramovich@epam.com>
2022-07-05 14:38:20 -0700

    Commit the solution

--- cmd/muxing/muxing.go
+++ cmd/muxing/muxing.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 	"os"
@@ -17,9 +18,50 @@ Feel free to drop gorilla.mux if you want and use any other solution available.
 main function reads host/port from env just for an example, flavor it following your taste
 */
 
+func getParamName(w http.ResponseWriter, r *http.Request) {
+	lParam := mux.Vars(r)["PARAM"]
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "Hello, %s!", lParam)
+}
+
+func getBad(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusInternalServerError)
+}
+
+func postData(w http.ResponseWriter, r *http.Request) {
+	lBody, lError := io.ReadAll(r.Body)
+	if lError != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "I got message:\n%s", string(lBody))
+}
+
+func postHeaders(w http.ResponseWriter, r *http.Request) {
+	lValue1Text := r.Header.Get("a")
+	lValue2Text := r.Header.Get("b")
+
+	lValue1, lError1 := strconv.Atoi(lValue1Text)
+	lValue2, lError2 := strconv.Atoi(lValue2Text)
+
+	if lError1 != nil || lError2 != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("a+b", strconv.Itoa(lValue1+lValue2))
+}
+
 // Start /** Starts the web server listener on given host and port.
 func Start(host string, port int) {
 	router := mux.NewRouter()
+
+	router.HandleFunc("/name/{PARAM}", getParamName).Methods("GET")
+	router.HandleFunc("/bad", getBad).Methods("GET")
+	router.HandleFunc("/data", postData).Methods("POST")
+	router.HandleFunc("/headers", postHeaders).Methods("POST")
 
 	log.Println(fmt.Printf("Starting API server on %s:%d\n", host, port))
 	if err := http.ListenAndServe(fmt.Sprintf("%s:%d", host, port), router); err != nil {
